ed4e275034c2ec10247b3bbcc1f2de989a8c2fb9
Drhackermedicina <HELLITONCECHINEL1@GMAIL.COM>
2025-06-08 01:08:14 -0300

    Remove pasta training para eliminar erro de generateStaticParams()

--- functions/index.js
+++ functions/index.js
@@ -9,8 +9,14 @@
 
 const {onRequest} = require("firebase-functions/v2/https");
 const {logger} = require("firebase-functions");
+const functions = require("firebase-functions");
 
 exports.apiExample = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send({ message: "Olá do backend!" });
+});
+
+// Define a região desejada (exemplo: us-central1)
+exports.backendFunction = functions.region("us-central1").https.onRequest((req, res) => {
+  res.send("Hello from us-central1!");
 });
